8e0c6fe0f733090b7af18c9bced010e2b5a6a438
AcapXasyraF <acapxasyrafx@yahoo.com>
2024-11-20 11:32:00 +0800

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,23 +1,20 @@
+import { defineNuxtConfig } from '@nuxt/bridge'
 import webpack from 'webpack'
 import PnpWebpackPlugin from "pnp-webpack-plugin";
 
-export default {
+export default defineNuxtConfig({
+  bridge: false,
   router: {
-    base: '/app/'
+    base: '/'
   },
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "MITS 2.0",
+    title: process.env.TARGET_APP_NAME || "MADOCS",
     htmlAttrs: {
       lang: "en",
     },
-    script:[
-      // {src:'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'},
-      // {src:'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'}
-
-      // Add For Sidebar Menu
+    script: [
       {
-        src: "/app/js/scripts.js",
+        src: "/js/scripts.js",
         type: "text/javascript"
       },
       {
@@ -45,24 +42,31 @@ export default {
       }
     ],
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-    { rel: 'stylesheet', href: 'https://pro.fontawesome.com/releases/v5.10.0/css/all.css' },
-    { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css' },
-    {
-      rel: "stylesheet",
-      href:
-        "https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
-    }
-
-  ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://pro.fontawesome.com/releases/v5.10.0/css/all.css' }
+    ],
+    // ToDo : Commenting for a while to see how the performance on FE and some libraries need to readjust where to be put
+    // meta: [
+    //   { charset: "utf-8" },
+    //   { name: "viewport", content: "width=device-width, initial-scale=1" },
+    //   { hid: "description", name: "description", content: "" },
+    //   { name: "format-detection", content: "telephone=no" },
+    // ],
+    // link: [
+    //   { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+    //   { rel: 'stylesheet', href: 'https://pro.fontawesome.com/releases/v5.10.0/css/all.css' },
+    //   { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css' },
+    //   {
+    //     rel: "stylesheet",
+    //     href:
+    //       "https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+    //   }
+    // ],
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     "@/assets/css/dataTables.bootstrap5.min.css",
     "@/assets/css/styles.css",
@@ -70,23 +74,16 @@ export default {
     "@/assets/css/richtext.min.css",
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    plugins: [
-      // { src: '~/plugins/vue-html2pdf', mode: 'client' }
-      { src: "@/plugins/vue-the-mask.js"},
-      { src: "@/plugins/vue-json-excel.js"},
+  plugins: [
+    { src: '@/plugins/vue-the-mask.js', mode: process.env.TARGET_MODERN || 'client' },
+    { src: '@/plugins/vue-json-excel.js', mode: process.env.TARGET_MODERN || 'client' },
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/bootstrap
     "bootstrap-vue/nuxt",
     "@nuxtjs/axios",
     "@nuxtjs/auth",
@@ -94,38 +91,39 @@ export default {
   ],
 
   sweetalert: {
-      confirmButtonColor: '#008B8B',
-      cancelButtonColor: '#ff7674'
+    confirmButtonColor: '#008B8B',
+    cancelButtonColor: '#ff7674'
   },
+
   axios: {
-    baseURL: "https://mits2be-aipl-704639301453.asia-southeast1.run.app/api/"
-    //baseURL:"http://192.168.1.15:8080/api/"
-    //  baseURL:"http://127.0.0.1:8000/api/"
-    // baseURL: "http://araken.asuscomm.com:8000/api/"
-    // baseURL: "https://mentari.moh.gov.my/point/api/"
-
+    baseURL: process.env.TARGET_API_BASE_URL || "http://127.0.0.1:8000/api/"
   },
 
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    publicPath: '/_nuxt/',
     babel: {
       compact: true,
     },
-    transpile: /@fullcalendar.*/, //this is needed
+    transpile: ['@fullcalendar'], // Set transpile as an array and include the necessary regex or strings
+    optimization: {
+      minimize: true,
+    },
 
     extend(config) {
       config.resolve.plugins.push(PnpWebpackPlugin);
       config.resolveLoader.plugins.push(PnpWebpackPlugin.moduleLoader(module));
     }
   },
+
   server: {
-    host:"0.0.0.0",
+    host: process.env.TARGET_HOST || "0.0.0.0",
     timing: {
-      total:true
+      total: true
     },
   },
-  ssr: false,
-  // target: 'development',
-  target: 'server',
-};
+
+  ssr: process.env.TARGET_SSR_DEPLOY || false,
+
+  target: process.env.TARGET_DEPLOY || 'server',
+  modern: process.env.TARGET_MODERN || 'client',  // Use modern build for modern browsers
+})
